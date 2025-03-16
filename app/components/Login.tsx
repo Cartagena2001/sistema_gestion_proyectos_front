59aa3db42a33d8bb12caabf37c702035c5e9cb66
Cartagena2001 <salvadorcartajena@hotmail.com>
2025-03-15 20:02:05 -0600

    change on login and sidebar and logout

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,40 +1,70 @@
-"use client"
+"use client";
 import { useState } from "react";
-import Link from "next/link";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { login, signup } from "../actions";
 
 export function Login() {
-  const [passwordShown, setPasswordShown] = useState(false);
-  const togglePasswordVisiblity = () => setPasswordShown((cur) => !cur);
 
   return (
     <section className="">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">  
-            </a>
-            <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
-                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                        Inicia sesión
-                    </h1>
-                    <form className="space-y-4 md:space-y-6" action="#">
-                        <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Tu correo electronico</label>
-                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required />
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Contraseña</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
-                        </div>
-                        <div className="flex items-center justify-between">
-                            <Button formAction={signup}>Registrate</Button>
-                            <Button formAction={login}>Ya tienes una cuenta? inicia session</Button>
-                        </div>
-                    </form>
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="#"
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900"
+        ></a>
+        <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Inicia sesión
+            </h1>
+            <form className="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Tu correo electronico
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  placeholder="name@company.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Contraseña
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div>
+                  <Button formAction={login} className="cursor-pointer">Iniciar sesión</Button>
                 </div>
-            </div>
+              </div>
+              <div className="w-full">
+                <span className="text-sm text-slate-600 font-light">
+                  No tienes cuenta aun? ingresa tu correo y registrate.
+                </span>
+                <Button formAction={signup} className="cursor-pointer">Registrate Ahora!</Button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </section>
   );
 }
